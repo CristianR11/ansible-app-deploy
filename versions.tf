cf20a986d8687b2cafccd0f531ea756960ccf6b8
CristianR11 <40369712+CristianR11@users.noreply.github.com>
2020-08-18 22:51:25 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,3 +1,8 @@
+provider "ibm" {
+  ibmcloud_api_key = "lFv_HdeW9JsLynBuQ6lDg8if0HvluIQJfEy51jLR3lut"
+  generation         = 2
+  region             = "eu-gb"
+}
 
 terraform {
   required_version = ">= 0.12"
