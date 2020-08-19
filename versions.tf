bbf48e0263582b5e5cfb9d64f7e265a41c173f13
CristianR11 <40369712+CristianR11@users.noreply.github.com>
2020-08-18 23:07:02 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,5 @@
-provider "skytap" {
-  ibmcloud_api_key = "lFv_HdeW9JsLynBuQ6lDg8if0HvluIQJfEy51jLR3lut"
-  generation         = 2
-  region             = "eu-gb"
+provider "ibm" {
+  region = "eu-gb"
 }
 
 terraform {
